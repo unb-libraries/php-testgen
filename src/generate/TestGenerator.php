7140ffcf0a235602f07a40c00b31290e6d87f554
Patrick Schilf <pschilf@unb.ca>
2020-01-10 12:07:42 -0400

    IN-198 Assign templates and output upon generating

--- src/generate/TestGenerator.php
+++ src/generate/TestGenerator.php
@@ -16,60 +16,23 @@ class TestGenerator {
   const TEMPLATE_ROOT = __DIR__ . '/../../templates/';
 
   /**
-   * Directory instance under which to store created test cases.
-   *
-   * @var Directory
-   *   A Directory instance.
-   */
-  protected $outputRoot;
-
-  /**
-   * Directory instance under which templates are stored.
-   *
-   * @var Directory
-   *   A Directory instance.
-   */
-  protected $templateRoot;
-
-  /**
-   * Getter for outputRoot.
-   *
-   * @return Directory
-   *   A Directory instance.
-   */
-  protected function outPutRoot() {
-    return $this->outputRoot;
-  }
-
-  /**
-   * Getter for templateRoot.
-   *
-   * @return Directory
-   *   A Directory instance.
-   */
-  protected function templateRoot() {
-    return $this->templateRoot;
-  }
-
-  /**
-   * Create a new TestGenerator instance.
-   *
-   * @param string $output_root
-   *   Path to the output root
-   * @param string $template_root
-   *   Path to the template root
-   */
-  public function __construct($output_root = self::OUTPUT_ROOT, $template_root = self::TEMPLATE_ROOT) {
-    $this->outputRoot = new Directory($output_root);
-    $this->templateRoot = new Directory($template_root);
-  }
-
-  /**
    * Generate test cases.
+   *
+   * @param Directory|string $output_root
+   *   Directory or path to directory into which to put generated files.
+   * @param Directory|string $template_root
+   *   Directory or path to directory which contains template files.
    */
-  public function generate() {
-    foreach ($this->templateRoot()->files() as $template) {
-      $template->copy($this->outPutRoot());
+  public function generate($output_root = self::OUTPUT_ROOT, $template_root = self::TEMPLATE_ROOT) {
+    if (\is_string($output_root)) {
+      $output_root= new Directory($output_root);
+    }
+    if (\is_string($template_root)) {
+      $template_root = new Directory($template_root);
+    }
+
+    foreach ($template_root->files() as $template) {
+      $template->copy($output_root);
     }
   }
 
