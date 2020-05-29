7dcd6760469965cb22c722f3f63e30c472b86d9d
Patrick Schilf <pschilf@unb.ca>
2020-05-29 11:52:49 -0300

    IN-198 Locating models
    Locate by file type and modal validation

--- src/Subject/SubjectModelLocator.php
+++ src/Subject/SubjectModelLocator.php
@@ -2,8 +2,9 @@
 
 namespace Tozart\Subject;
 
+use Tozart\os\FileTypeFilter;
 use Tozart\os\Locator;
-use Tozart\os\YamlFilter;
+use Tozart\os\SubjectModelValidationFilter;
 
 /**
  * Locator to locate files that contain model definitions.
@@ -17,10 +18,19 @@ class SubjectModelLocator extends Locator {
    *
    * @param array $directories
    *   An array of directories or paths.
+   * @param array $file_types
+   *   An array of file types indicating the
+   *   file format which models should be declared in.
    */
-  public function __construct(array $directories) {
-    parent::__construct($directories, [new YamlFilter()]);
-
+  public function __construct(array $directories, $file_types = []) {
+    $filters = [];
+    if (!empty($file_types)) {
+      $filters[] = new FileTypeFilter($file_types);
+      // TODO: Allow only one file_type.
+      // TODO: Create file type class to associate several file extensions with one file type.
+      $filters[] = new SubjectModelValidationFilter($file_types[array_keys($file_types)[0]]);
+    }
+    parent::__construct($directories, $filters);
   }
 
 }
