1ac7951591aabc44d77591fad1bac925667e0467
Patrick Schilf <pschilf@unb.ca>
2020-07-17 16:45:22 -0300

    IN-198 Update discovery test

--- tests/phpunit/src/Discovery/Discovery.php
+++ tests/phpunit/src/Discovery/Discovery.php
@@ -4,6 +4,22 @@ namespace Tozart\Test\Discovery;
 
 use Tozart\Discovery\DiscoveryBase;
 
+/**
+ * Test discovery implementation.
+ *
+ * @package Tozart\Test\Discovery
+ */
 class Discovery extends DiscoveryBase {
+
+  /**
+   * {@inheritDoc}
+   */
+  public function findBy($key) {
+    $finds = [];
+    foreach ($this->discover() as $dir => $files) {
+      $finds += $files;
+    }
+    return $finds;
+  }
 
 }
