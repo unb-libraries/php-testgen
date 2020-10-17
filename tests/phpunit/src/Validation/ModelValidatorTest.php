c7c4cf50aaa399a2260889c98a4965891f855246
Patrick Schilf <pschilf@unb.ca>
2020-10-16 22:52:09 -0300

    IN-198 Test model validator

---
+++ tests/phpunit/src/Validation/ModelValidatorTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tozart\Test\Validation;
+
+use Tozart\Test\Subject\ExampleSubject;
+use Tozart\Validation\ModelValidator;
+
+/**
+ * Test the ModelValidator class.
+ *
+ * @package Tozart\Test\Validation
+ */
+class ModelValidatorTest extends SpecificationValidatorTest {
+
+  /**
+   * {@inheritDoc}
+   */
+  protected function createValidator() {
+    return new ModelValidator($this->createParser());
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function fileProvider() {
+    $subject_class = ExampleSubject::class;
+    return [
+      // Should fail because type is not given.
+      [$this->createFile('model_0', 'test', "type: ''"), FALSE],
+      // Should fail because type is not a string.
+      [$this->createFile('model_1', 'test', "type: 123"), FALSE],
+      // Should fail because class does not implement SubjectInterface.
+      [$this->createFile('model_2', 'test', "type: 'test'\nclass: '\Tozart\Test\ValidationModelValidatorTest'"), FALSE],
+      // Should pass because requirements and options are optional.
+      [$this->createFile('model_3', 'test', "type: 'test'\nclass: '{$subject_class}'"), TRUE],
+      // Should fail because requirements must be an array.
+      [$this->createFile('model_4', 'test', "type: 'test'\nclass: '{$subject_class}'\nrequirements: ''"), FALSE],
+      // Should pass because options are optional.
+      [$this->createFile('model_5', 'test', "type: 'test'\nclass: '{$subject_class}'\nrequirements: [requiredProperty]"), TRUE],
+      // Should fail because options is not an array.
+      [$this->createFile('model_6', 'test', "type: 'test'\nclass: '{$subject_class}'\nrequirements: [requiredProperty]\noptions: ''"), FALSE],
+      // Should fail because each option must provide a default value.
+      [$this->createFile('model_7', 'test', "type: 'test'\nclass: '{$subject_class}'\nrequirements: [requiredProperty]\noptions: [optionalProperty]"), FALSE],
+      // Should pass.
+      [$this->createFile('model_8', 'test', "type: 'test'\nclass: '{$subject_class}'\nrequirements: [requiredProperty]\noptions: {optionalProperty:2}"), TRUE],
+    ];
+  }
+
+}
